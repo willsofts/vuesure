66edc7d69c1262c3672d9502e3968a3867a2ce1c
tassun <32536248+tassun@users.noreply.github.com>
2024-09-20 12:00:30 +0700

    detect only message type=bc

--- src/assets/js/bc.js
+++ src/assets/js/bc.js
@@ -10,17 +10,19 @@ export function startReceiveBroadcast(chat_url) {
         socket = io(chat_url);
         socket.on('broadcast-message', function(msg) {
             console.log("broadcast-message:",msg);
-            let div = $("<div class='bc-layer'></div>");
-            let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
-            link.on("click",() => { div.remove(); });
-            let span = $("<span></span>").html(msg.message);
-            let body = $("body");
-            let isz = body.find("div.bc-layer").length;
-            if(isz > 0 && isz < 10) {
-                let bottom = isz * 50;
-                div.css({bottom: bottom});
+            if(msg.type == "bc") {
+                let div = $("<div class='bc-layer'></div>");
+                let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
+                link.on("click",() => { div.remove(); });
+                let span = $("<span></span>").html(msg.message);
+                let body = $("body");
+                let isz = body.find("div.bc-layer").length;
+                if(isz > 0 && isz < 10) {
+                    let bottom = isz * 50;
+                    div.css({bottom: bottom});
+                }
+                div.append(link).append(span).appendTo(body);
             }
-            div.append(link).append(span).appendTo(body);
         });
     }
 }
