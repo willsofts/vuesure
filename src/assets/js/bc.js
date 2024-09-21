6ad9cca5f4e748d218f7dba37266668bc0079a09
tassun <32536248+tassun@users.noreply.github.com>
2024-09-21 08:45:16 +0700

    Update bc.js

--- src/assets/js/bc.js
+++ src/assets/js/bc.js
@@ -1,6 +1,7 @@
 import $ from "jquery"
 import io from "socket.io-client";
 import { getChatUrl } from "@willsofts/will-app";
+import { openPage } from "./login.util";
 
 let socket = null;
 export function startReceiveBroadcast(chat_url) {
@@ -22,6 +23,22 @@ export function startReceiveBroadcast(chat_url) {
                     div.css({bottom: bottom});
                 }
                 div.append(link).append(span).appendTo(body);
+            } else if(msg.type == "app" && msg.app) {
+                //try support open app directly
+                //app info = { programid: "?", url: "?", parameters: "?", progpath: "?" }
+                let div = $("<div class='bc-layer'></div>");
+                let link = $("<a href=\"javascript:void(0)\" class=\"bc-close\" aria-label=\"close\"></a>").html("<em class='fa fa-close'></em>");
+                link.on("click",() => { div.remove(); });
+                let alink = $("<a href=\"javascript:void(0)\" class=\"app-text\"></a>").html(msg.message);
+                alink.on("click",() => { openPage(msg.app); link.trigger("click"); });
+                let span = $("<span></span>").append(alink);
+                let body = $("body");
+                let isz = body.find("div.bc-layer").length;
+                if(isz > 0 && isz < 10) {
+                    let bottom = isz * 50;
+                    div.css({bottom: bottom});
+                }
+                div.append(link).append(span).appendTo(body);                
             }
         });
     }
